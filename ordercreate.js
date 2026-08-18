6eaf877f3bd27af3608ca5a9f20c4b8d688b4f28
Faizan-15 <m.faizanashraf9@gmail.com>
2026-08-18 12:51:05 +0500

    Initiating New Functionalities

---
+++ ordercreate.js
@@ -0,0 +1,37 @@
+function fetchproducts() {
+    // Loop through the products array and create HTML elements for each product
+    const xhttp = new XMLHttpRequest();
+    linkproducts = `https://oracleapex.com/ords/faizan05/hr/faizanpro/`;
+    xhttp.open("GET", linkproducts, true);
+    xhttp.send();
+    xhttp.onload = () => {
+        if (xhttp.status === 200) {
+            const response = JSON.parse(xhttp.responseText);
+            const products = response.items;
+            console.log(products);
+            const productsContainer = document.getElementById('products-container');
+            console.log(productsContainer);
+            productsContainer.innerHTML = ''; // Clear previous content
+
+            for (let i = 0; i < products.length; i++) {
+                const product = products[i];
+                console.log(product);
+                productElement = document.createElement('div');
+                productElement.className = 'productcards';
+                console.log(productElement);
+                productElement.innerHTML = `
+                    <h3>${product.productname}</h3>
+                    <p>Price: ${product.price}</p>
+                    <button>Add to Cart</button>
+                `;
+                productsContainer.appendChild(productElement);
+            }
+        }
+    }
+}
+
+function addtocart(productname, price) {
+    sessionStorage.setItem("productname", productname);
+    sessionStorage.setItem("price", price);
+    
+}
